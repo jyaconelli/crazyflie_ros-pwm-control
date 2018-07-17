f4b71d40b0223733c329c9ea42c71817b29ed715
Joseph Yaconelli <josephyaconelli@gmail.com>
2018-07-17 10:01:07 -0700

    more to mpc

--- src/crazyflie_mpc/src/MPController.py
+++ src/crazyflie_mpc/src/MPController.py
@@ -10,6 +10,7 @@ from dynamics_crazyflie_linearized import CrazyFlie
 from dynamics import *
 import numpy as np
 
+
 from utils_data import *
 
 
@@ -25,7 +26,8 @@ from crazyflie_driver.msg import MotorControl, GenericLogData, LogBlock
 rp = rospkg.RosPack()
 
 #model_path = os.path.join(rp.get_path("crazyflie_mpc"), "src", "2018-07-12_general_temp.pth")
-model_path = os.path.join(rp.get_path("crazyflie_mpc"), "src", "clean_flie_hover_long.pth")
+#model_path = os.path.join(rp.get_path("crazyflie_mpc"), "src", "clean_flie_hover_long.pth")
+model_path = os.path.join(rp.get_path("crazyflie_mpc"), "src", "9_state_200_w.pth")
 #model_path = os.path.join(rp.get_path("crazyflie_mpc"), "src", "large_state_pnn.pth")
 
 x_prev = np.zeros([12])
@@ -45,23 +47,15 @@ def callback(data):
   global totalReceived
   totalReceived += 1
   #print "\n\n\n\ncallback hz: ", (totalReceived / (millis() * 0.001))
-  #print "\n\n\n\ntime: ", millis()
-  #rospy.loginfo(rospy.get_caller_id() + "I heard this %s %s %s %s %s %s %s", data.values[0],
-  #data.values[1],
-  #data.values[2],
-  #data.values[3],
-  #data.values[4],
-  #data.values[5],
-  #data.values[6])
 
-  p = np.zeros([1])
-  p[0] = data.values[2]
+  #p = np.zeros([1])
+  #p[0] = data.values[2]
 
-  l = np.zeros([1])
-  l[0] = data.values[1]
+  #l = np.zeros([1])
+  #l[0] = data.values[1]
 
-  a = np.zeros([1])
-  a[0] = data.values[0]
+  #a = np.zeros([1])
+  #a[0] = data.values[0]
 
   #pwms = unpack_cf_pwm(p)
   #imu = unpack_cf_imu(l,a)
@@ -104,7 +98,7 @@ def nodecontroller():
   print('...Objective Function Initialized')
 
 ################################ MPC ####################################
-  mpc1 = MPController(newNN, crazy, dt_x, dt_u, origin_minimizer, N=10, T=3, variance = .00003)
+  mpc1 = MPController(newNN, crazy, dt_x, dt_u, origin_minimizer, N=10, T=3, variance = 0.0003)
   print('...MPC Running')
 
 
@@ -113,13 +107,23 @@ def nodecontroller():
 
 
   while  millis() < testTime:
-    while not rospy.is_shutdown():
+    if not rospy.is_shutdown():
+#        key = getkey()
+#        if key == 'q':
+#          msg = MotorControl()
+#          msg.m1 = 0
+#          msg.m2 = 0
+#          msg.m3 = 0
+#          msg.m4 = 0
+#          pub.publish(msg)
+#          quit()
+
         # get current state
         #x_prev = parseFlie   # WRITE DIS
         #receieved = millis()
 
         # generate new u
-        u = np.zeros([4])
+        #u = np.zeros([4])
         #if begunReceiving:
         u = mpc1.update(x_prev)   # UPDATE SHOULD RETURN PWMs
         #print("U UPDATEED: ", u)
